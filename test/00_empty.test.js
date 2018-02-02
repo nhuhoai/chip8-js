b14508edf630998409d4b5ea884d7a7144997077
Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
2018-02-02 23:15:00 +0100

    Add empty test (example).

---
+++ test/00_empty.test.js
@@ -0,0 +1,18 @@
+/**
+ *  @file       00_empty.test.js
+ *  @version    0.1.0
+ *  @date       2018-02-02
+ *  @author     Nhu-Hoai Robert VO <nhuhoai.vo@nhuvo.ch>
+ *  @copyright  Nhu-Hoai Robert VO
+ *  @brief      Empty unit test
+ */
+
+describe('Empty unit test', function() {
+  it('Empty test', function() {
+    expect(null).toBeNull();
+  });
+
+  it('One equals 1', function() {
+    expect(1).toEqual(1);
+  });
+});
